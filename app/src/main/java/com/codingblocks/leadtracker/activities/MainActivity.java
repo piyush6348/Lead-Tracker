061ccfd922551a00524ac5a4e16ef073ae7c5103
Nimit Aggarwal <nimitagg95@gmail.com>
2017-07-27 18:12:33 +0530

    gradle dependencies

--- app/src/main/java/com/codingblocks/leadtracker/activities/MainActivity.java
+++ app/src/main/java/com/codingblocks/leadtracker/activities/MainActivity.java
@@ -1,13 +1,40 @@
-package com.codingblocks.leadtracker;
+package com.codingblocks.leadtracker.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.codingblocks.leadtracker.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
     }
+
+    private void setUpDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItem(item);
+                return true;
+            }
+        });
+    }
+
+
 }

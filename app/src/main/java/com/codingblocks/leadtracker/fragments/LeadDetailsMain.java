2c81598368d3b42737fda1841c5de58ce2c742ed
piyush6348 <piyush6348@gmail.com>
2017-07-28 13:47:32 +0530

    Leads Detail Setup done

--- app/src/main/java/com/codingblocks/leadtracker/fragments/LeadDetailsMain.java
+++ app/src/main/java/com/codingblocks/leadtracker/fragments/LeadDetailsMain.java
@@ -2,9 +2,12 @@ package com.codingblocks.leadtracker.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
+
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +23,8 @@ public class LeadDetailsMain extends Fragment {
     private ViewPager leadsDetailsViewPager;
     private LeadsDetailsAdapter leadsDetailsAdapter;
     private PagerTitleStrip pagerTitleStrip;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
     public LeadDetailsMain() {
         // Required empty public constructor
     }
@@ -30,11 +35,18 @@ public class LeadDetailsMain extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View mainView= inflater.inflate(R.layout.fragment_lead_details_main, container, false);
-        leadsDetailsViewPager= (ViewPager) mainView.findViewById(R.id.leads_details_viewpager);
-        pagerTitleStrip= (PagerTitleStrip) mainView.findViewById(R.id.leads_details_viewpager_fragment_title);
+
+        initialize(mainView);
         leadsDetailsAdapter= new LeadsDetailsAdapter(getChildFragmentManager());
         leadsDetailsViewPager.setAdapter(leadsDetailsAdapter);
+        tabLayout.setupWithViewPager(leadsDetailsViewPager);
         return mainView;
+    }
+
+
+    private void initialize(View mainView) {
+        leadsDetailsViewPager= (ViewPager) mainView.findViewById(R.id.leads_details_viewpager);
+        tabLayout = (TabLayout) mainView.findViewById(R.id.tabs);
     }
 
 }
